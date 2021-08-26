b001567b6b13093a75e3968cb06d274b9c9d22ca
lizt <1196994917@qq.com>
2021-08-26 22:59:03 +0800

    抽奖雏形

---
+++ fronted/app/src/views/manage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="Home">
+    <div class="box">
+      <div class="a">aaaa</div>
+      <div class="b"></div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+     
+    }
+
+  },
+  methods: {
+   
+  }
+}
+</script>
+
+<style >
+.box{
+  width: 500px;
+  height: 500px;
+  background-color: antiquewhite;
+  display: flex;
+}
+.a{
+  width: 50px;
+  height: 50px;
+  background-color: aquamarine;
+}
+.b{
+  width: 150px;
+  height: 30px;
+  background-color: coral;
+}
+</style>
